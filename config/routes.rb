d92f0c852adf582d77844765ebcdb6c4c6bef68f
github username <github email address>
2020-05-18 01:24:38 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#The GET request to the route “/posts/:id"
+#Is mapped to the PostsController & it’s action show
+#The parameter will be passed to the controller's show action
+#This will render the about.html.erb view template
+
+##get "/posts/:id", to: "posts#show"
+
+#We can refractor the above route via
+#The  resources method
+#The option only: - selects which of the 7 RESTful routes we care about
+ resources :posts, only: :show
+ 
 end
